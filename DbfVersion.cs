e06c3cb39a748df3f29abaa5d7e4cb398ac4e5b9
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2018-08-04 13:26:16 +0200

    Marked some versions as done.

--- DbfVersion.cs
+++ DbfVersion.cs
@@ -13,15 +13,15 @@ namespace dBASE.NET
 	{
 		Unknown = 0,
 		FoxBase = 0x02,
-		FoxBaseDBase3NoMemo = 0x03,
-		VisualFoxPro = 0x30,
-		VisualFoxProWithAutoIncrement = 0x31,
+		FoxBaseDBase3NoMemo = 0x03,            // DONE
+		VisualFoxPro = 0x30,                   // DONE
+		VisualFoxProWithAutoIncrement = 0x31,  // DONE
 		dBase4SQLTableNoMemo = 0x43,
 		dBase4SQLSystemNoMemo = 0x63,
-		FoxBaseDBase3WithMemo = 0x83,
-		dBase4WithMemo = 0x8B,
+		FoxBaseDBase3WithMemo = 0x83,          // DONE
+		dBase4WithMemo = 0x8B,                 // DONE
 		dBase4SQLTableWithMemo = 0xCB,
-		FoxPro2WithMemo = 0xF5,
+		FoxPro2WithMemo = 0xF5,                // DONE
 		FoxBASE = 0xFB
 	}
 }
