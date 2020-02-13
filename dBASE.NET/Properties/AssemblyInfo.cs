6861ace8554f97e073ff7df92cadbdad28abd3f2
Jan Zahradník <jan.zahradnik@gmail.com>
2020-02-13 14:22:10 +0100

    podpora netstandard2.0

--- dBASE.NET/Properties/AssemblyInfo.cs
+++ dBASE.NET/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("dBASE.NET")]
-[assembly: AssemblyDescription("dBASE.NET is a .NET class library used to read and write FoxBase, dBASE III and dBASE IV .dbf files. The Dbf class reads fields and records from a .dbf file. These fields and records can then be accessed as lists and looped over.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Independent Software / Alexander van Oostenrijk")]
-[assembly: AssemblyProduct("dBASE.NET")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,19 +9,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6d931bbd-e331-4a20-90e8-ff7bb2a46e8f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: InternalsVisibleTo("dBASE.NET.Tests, PublicKey=" +
                               "0024000004800000940000000602000000240000525341310004000001000100158d9c3dde3f3f" +
