2d3151eebb9e2bbd18fae75af9a5e87550efbe83
Járdi Mihály <fusyo88@hotmail.com>
2021-06-14 13:57:17 +0200

    Added entity casting to DbfRecord. Added attribute that allows us to map a property to a field.

---
+++ dBASE.NET/DbfFieldAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dBASE.NET
+{
+    /// <summary>
+    /// Defines a field within an entity
+    /// </summary>
+    public class DbfFieldAttribute : Attribute
+    {
+        /// <summary>
+        /// Name of the property in the DBF
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Create an attribute to be able to bind to a DBF field.
+        /// </summary>
+        /// <param name="name">
+        ///     The name of the field. Can be omitted, in that case it takes the name of the property.
+        /// </param>
+        public DbfFieldAttribute([CallerMemberName] string name = null)
+        {
+            Name = name;
+        }
+    }
+}
